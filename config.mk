5eef0016c97aa6d103c7f6964ff39c4d2aeb0648
Vinh VO <vinh.vo30@gmail.com>
2021-10-09 22:42:32 +0700

    Show almost everything

--- config.mk
+++ config.mk
@@ -7,13 +7,13 @@ VERSION = 0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
-LDFLAGS  = -L$(X11LIB) -s
+LDFLAGS  = -L$(X11LIB) -s -lkvm
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm
 LDLIBS   = -lX11
